dcac6ac5aa38e2d5c0a96741622ac090fb931a77
JCB <jcbukenya@gmail.com>
2021-02-26 12:27:27 +0000

    edit and show collections

--- routes/collections.js
+++ routes/collections.js
@@ -4,6 +4,20 @@ const Song = require("../models/songs");
 const Collection = require("../models/collections");
 const middleware = require("../middleware");
 
+router.get("/", middleware.isLoggedIn, async(req,res,next)=>{
+	try{
+		const allCollections = await Collection.find({},null,{sort:{name: 1}}).populate({path: 'songs'})
+				
+		res.render("collections/show", {collections:allCollections});
+	}catch(err)
+	{			
+			throw new Error(err);		
+	}
+	
+	//res.render("collections/show");
+	
+});
+
 router.get("/new", async(req,res,next)=>{	
 	
 	res.render("collections/new");
@@ -20,6 +34,28 @@ router.post("/", middleware.isLoggedIn, async(req,res,next)=>{
 					}else{
 						res.redirect('song/new');					
 					}		
+	})
+})
+
+router.get('/:id/edit', middleware.isLoggedIn, async(req,res,next)=>{	
+	try{
+		const collection = await Collection.findById(req.params.id);
+		res.render('collections/edit', {collection:collection});
+		
+	}catch(err){
+		throw new Error(err)
+	}
+})
+
+router.put('/:id', middleware.isLoggedIn, async(req,res,next)=>{	
+	
+	Collection.findByIdAndUpdate(req.params.id, req.body.collection, function(err, updatedCollection){
+		if(err){
+			res.redirect("/collections");
+		}else{	
+			req.flash('success', 'Collection updated.');
+			res.redirect("/collections");
+		}
 	})
 })
 
