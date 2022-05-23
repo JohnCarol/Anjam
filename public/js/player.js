6e2f5257b1fa13467cd4a650d755dd0c4d1584e2
JCB <jcbukenya@gmail.com>
2022-05-23 15:04:40 +0000

    remove downloaded

--- public/js/player.js
+++ public/js/player.js
@@ -1,23 +1,68 @@
 $(function(){
-	$('.play').on('click', function() {
-	  
+	
+	const audios = [];
+	let i = 0;
+	let wavesurfer;
+	
+	$(".player").each(function(){
+		
+		//$(".player")[i].pause();
+		//wavesurfer[i].stop()
+		
+		 wavesurfer = WaveSurfer.create({
+    	container: '#waveform_' +i});
+		
+		audios.push(wavesurfer);
+		i++;
+		
+	})	
+	
+	//console.log(audios);
+	
+	
+	$('.play').on('click', function() {		
+	
+		  
 	let playerName = "player_" + $(this).attr('name');   
+	let fileName = 	$('#' + playerName).attr('src');
     let seekBar = "seekbar_" + $(this).attr('name');
+	let wavesurfer = audios[$(this).attr('name')];	
+
+	wavesurfer.load(fileName);
+	
 		
 	$(".mixbuttons").each(function(){			
 			
 		$(this).closest('.thumbnail').removeClass("playing");
 			
 	})
-	let i = 0;
+	
+		//console.log($(this).attr('name'));
+		$("#waveform_"+$(this).attr('name')).show();
+		wavesurfer.on('ready', function () {
+    	wavesurfer.play();
+		
+		
+	});		
+	
+		
+		
+	i = 0;
 	$(".player").each(function(){
 		
-		$(".player")[i].pause();
+		//$(".player")[i].pause();
+		//wavesurfer[i].stop()
+		//i++;
+		if(audios[i].isPlaying()){			
+			
+			audios[i].stop();
+			$("#waveform_"+i).hide();
+		}
 		i++;
-		
 	})	
 		
-    document.getElementById(playerName).play();
+    //document.getElementById(playerName).play();
+		
 		
 	$(this).closest('.thumbnail').addClass("playing");	
 		
@@ -25,7 +70,7 @@ $(function(){
     $('#'+ seekBar).attr("value", this.currentTime / this.duration);
     
     //let progressBar = document.getElementById("seekbar");
-		console.log(seekBar);
+		//console.log(seekBar);
     let progressBar = document.getElementById(seekBar);
 	progressBar.addEventListener("click", seek);
     
@@ -40,14 +85,20 @@ $(function(){
 });
 
 	$('.pause').on('click', function() {
-			let name = "player_" + $(this).attr('name');
+		let name = "player_" + $(this).attr('name');
 		document.getElementById(name).pause();
+		let wavesurfer = audios[$(this).attr('name')];
+		wavesurfer.playPause();
+
 	});
 	
 	$('.stop').on('click', function() {
 			let name = "player_" + $(this).attr('name');
-		console.log(name);
+		//console.log(name);
 		document.getElementById(name).pause();
 		document.getElementById(name).currentTime = 0;
+		let wavesurfer = audios[$(this).attr('name')];		
+		$("#waveform_"+$(this).attr('name')).hide();
+		wavesurfer.stop();
 	});
 });
--- public/js/player_old.js
+++ public/js/player_old.js
@@ -1,51 +1,23 @@
 $(function(){
-	
-	
-	$('.play').on('click', function() {		
-	
-		  
+	$('.play').on('click', function() {
+	  
 	let playerName = "player_" + $(this).attr('name');   
-	let fileName = 	$('#' + playerName).attr('src');
     let seekBar = "seekbar_" + $(this).attr('name');
-	let wavesurfer = WaveSurfer.create({
-    	container: '#waveform_' + $(this).attr('name')
-	});
-		
-	//let i = 0;
-	//let wavesurfer = [];
-	
-	//$(".player").each(function(){
-		
-	//	wavesurfer[i] = WaveSurfer.create({
-    	//container: '#waveform_' + i
-	//});
-	//	i++;
-		
-	//})	
-		
-	
-
-	wavesurfer.load(fileName);
-	
 		
 	$(".mixbuttons").each(function(){			
 			
 		$(this).closest('.thumbnail').removeClass("playing");
 			
 	})
-	i = 0;
+	let i = 0;
 	$(".player").each(function(){
 		
-		//$(".player")[i].pause();
-		//wavesurfer[i].stop()
-		//i++;
+		$(".player")[i].pause();
+		i++;
 		
 	})	
 		
-    //document.getElementById(playerName).play();
-		wavesurfer.on('ready', function () {
-    wavesurfer.play();
-});
+    document.getElementById(playerName).play();
 		
 	$(this).closest('.thumbnail').addClass("playing");	
 		
@@ -53,7 +25,7 @@ $(function(){
     $('#'+ seekBar).attr("value", this.currentTime / this.duration);
     
     //let progressBar = document.getElementById("seekbar");
-		//console.log(seekBar);
+		console.log(seekBar);
     let progressBar = document.getElementById(seekBar);
 	progressBar.addEventListener("click", seek);
     
@@ -77,6 +49,5 @@ $(function(){
 		console.log(name);
 		document.getElementById(name).pause();
 		document.getElementById(name).currentTime = 0;
-		wavesurfer.stop();
 	});
 });
